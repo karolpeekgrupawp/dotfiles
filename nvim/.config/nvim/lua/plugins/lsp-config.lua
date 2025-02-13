5bd15e0c559c833c2b53dfe00d7049c2d95e4d8b
kpeek <karol.peek@grupawp.pl>
2025-02-13 06:40:57 +0100

    golangcilint langserver

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -31,6 +31,21 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
+			local configs = require("lspconfig/configs")
+			if not configs.golangcilsp then
+				configs.golangcilsp = {
+					default_config = {
+						cmd = { 'golangci-lint-langserver' },
+						root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+						init_options = {
+							command = { "golangci-lint", "run", "--out-format", "json", "--issues-exit-code=1" },
+						}
+					},
+				}
+			end
+			lspconfig.golangci_lint_ls.setup {
+				filetypes = { 'go', 'gomod' }
+			}
 			lspconfig.lua_ls.setup({})
 			lspconfig.gopls.setup({
 				capabilities = capabilities,
